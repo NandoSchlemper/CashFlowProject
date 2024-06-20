3e5f2cb54328f47a5711cef85b16ae943db7c0c6
Fernando Meurer Schlemper <129208500+BekaODeus@users.noreply.github.com>
2024-06-20 07:54:30 -0300

    nheco

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
-    let tup = (10, 20);
-
-    let val1 = tup.1;
-
+async fn home() -> impl Responder {
+    HttpResponse::Ok().body("Bruh")
 }
 
-
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let bind_address = "127.0.0.1:8080";
+    HttpServer::new(|| App::new().service(web::scope("/").route("/", web::get().to(home))))
+        .bind(bind_address)?
+        .run()
+        .await
+}
