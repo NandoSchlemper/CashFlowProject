c9cdb186a8ede42bbbe4628fc53b739b4b3a706e
NandoSchlemper <haisekun01@gmail.com>
2024-06-22 07:43:08 -0300

    Nruh

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
+// Handlerr
 async fn home() -> impl Responder {
-    HttpResponse::Ok().body("Bruh")
+    HttpResponse::Ok().message_body("Hey")
 }
 
+
+// Roteamento
+// Define como as requisições são mapeadas p/ handlers especificos com base na URL
+// E metodos HTTP
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let bind_address = "127.0.0.1:8080";
-    HttpServer::new(|| App::new().service(web::scope("/").route("/", web::get().to(home))))
+    println!("Server instanciado na porta: {}", bind_address);
+    HttpServer::new(|| App::new()
+            .route("/", web::get().to(home)))
         .bind(bind_address)?
         .run()
         .await
 }
+
+// Utilizar Actix-files para handle de arquivos estáticos (HTML + CSS)
